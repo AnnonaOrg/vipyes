85b7e516fb5413115dd3177d58498284cf34bb3b
noreply <noreply@yourdomain.com>
2024-07-10 17:11:40 +0800

    v0.0.1 debug

--- features/start/start.go
+++ features/start/start.go
@@ -6,11 +6,16 @@ import (
 	tele "gopkg.in/telebot.v3"
 )
 
-// Command: /start <PAYLOAD>
-func Onstart(c tele.Context) error {
-	return nil
-}
-
 func init() {
 	features.RegisterFeature("/start", Onstart)
+}
+
+// Command: /start <PAYLOAD>
+func Onstart(c tele.Context) error {
+
+	menu := &tele.ReplyMarkup{ResizeKeyboard: true}
+	btnAddToChat := menu.URL("点击加我入群", "http://t.me/vipYesBot?startgroup=botstart")
+	menu.Reply(menu.Row(btnAddToChat))
+	c.Send("Vip Yes!", menu)
+	return nil
 }
