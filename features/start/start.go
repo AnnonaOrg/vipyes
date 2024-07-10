c57d9bd3d2cc1fd566c9014d49d7e880ab4a7f8b
noreply <noreply@yourdomain.com>
2024-07-10 21:35:35 +0800

    v0.0.1 debug

--- features/start/start.go
+++ features/start/start.go
@@ -15,6 +15,6 @@ func Onstart(c tele.Context) error {
 	menu := &tele.ReplyMarkup{ResizeKeyboard: true}
 	btnAddToChat := menu.URL("点击加我入群", "http://t.me/vipYesBot?startgroup=botstart")
 	menu.Inline(menu.Row(btnAddToChat))
-	c.Send("Vip Yes!", menu)
+	c.Send("vip Yes! 自动删除群内非会员消息.", menu)
 	return nil
 }
